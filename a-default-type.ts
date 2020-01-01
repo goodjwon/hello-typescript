93c5f6f50066fcbbf1715eb1b30a7911024f5320
goodjwon <jwon76@naver.com>
2020-01-02 07:45:28 +0900

    기본타입 선언 완료

--- a-default-type.ts
+++ a-default-type.ts
@@ -36,3 +36,41 @@ console.log(x[0].substr(1))
 enum Color {Red, Green, Blue}
 
 let c:Color = Color.Green;
+
+
+//AnyType
+let notSure: any = 4;
+notSure = "문자일수 있다."
+notSure = false;
+
+/* 오류발생
+let prettySure: Object = 4;
+prettySure.toFixed();
+*/
+
+let any_list: any[] = [1, true, "free"]
+any_list[1] = 100;
+
+
+function warnUser(): void{
+    alert("This is my warning message");
+}
+
+//undefied or null 만 할당 가능.
+let unsuable: void = undefined;
+
+//undefied 와 null은 다른 서브 타입이다.
+let u: undefined = undefined;
+let n: null = null;
+
+//naver 반환이 가능한 경우 
+function error(message: string): never{
+    throw new Error(message);
+}
+
+
+//type assertions 타입선언.
+let someValue: any = "this is a string";
+
+let strLength: number = (<string>someValue).length;
+
