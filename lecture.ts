edd3506a07db44e19b6182e1961d8529494165e0
BuildTools <unconfigured@null.spigotmc.org>
2020-01-11 12:52:43 +0900

    구구단만들기

---
+++ lecture.ts
@@ -0,0 +1,48 @@
+console.log('hello typescript');
+console.log('hello javascirpt');
+const abc: number = 1;
+console.log(abc);
+
+
+let numberOne = Math.ceil(Math.random()*9);
+let numberTwo = Math.ceil(Math.random()*9);
+let result = numberOne * numberTwo;
+
+const word = document.createElement('div');
+word.textContent = `${numberOne} 곱하기 ${numberTwo}`;
+document.body.append(word);
+
+const form = document.createElement('form');
+document.body.append(form);
+
+const input = document.createElement('input');
+input.type = 'number'
+document.body.append(input);
+
+
+const button = document.createElement('button');
+button.textContent = '정답은?ㄴ';
+form.append(button);
+
+const resultDiv = document.createElement('div');
+document.body.append(resultDiv);
+
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    if(result === Number(input.value)) {
+        resultDiv.textContent = '딩동댕';
+        numberOne = Math.ceil(Math.random()*9)
+        numberTwo = Math.ceil(Math.random()*9)
+        result = numberOne * numberTwo;
+        word.textContent = `${numberOne} 곱하기 ${numberTwo} 는?`
+    }else{
+        resultDiv.textContent = '땡';
+        input.value = '';
+        input.focus();
+    }
+})
+
+
+
+
