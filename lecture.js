56b14dbbd231389b1c1e6319c23f27ed03d6af91
goodjwon <jwon76@naver.com>
2020-11-20 07:45:11 +0900

    .

--- lecture.js
+++ lecture.js
@@ -21,7 +21,7 @@ document.body.append(resultDiv);
 form.addEventListener('submit', function (e) {
     e.preventDefault();
     if (result === Number(input.value)) {
-        resultDiv.textContent = '딩동댕';
+        resultDiv.textContent = '딩동댕동.';
         numberOne = Math.ceil(Math.random() * 9);
         numberTwo = Math.ceil(Math.random() * 9);
         result = numberOne * numberTwo;
